6bdf1715ace73eabf4f980686f53f68f5f0d3040
Nienorloth <nienorloth@gmail.com>
2019-11-13 16:58:04 -0600

    Updates

---
+++ precache-manifest.f0eb41983e294dc1b3fb1eec7cd51a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2065ab537efe5e4e3562c56b0356aea3",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "904caf1ecaaca8ed444d",
+    "url": "/Portafolio/static/css/main.8b2b2884.chunk.css"
+  },
+  {
+    "revision": "fc461e4a67800bc54a84",
+    "url": "/Portafolio/static/js/2.0abf5e3f.chunk.js"
+  },
+  {
+    "revision": "904caf1ecaaca8ed444d",
+    "url": "/Portafolio/static/js/main.15e31212.chunk.js"
+  },
+  {
+    "revision": "0dceec934d17b8d03d71",
+    "url": "/Portafolio/static/js/runtime~main.9747bc56.js"
+  },
+  {
+    "revision": "c7b9d22085c55518e2f84d39724e9acd",
+    "url": "/Portafolio/static/media/forest.c7b9d220.jpg"
+  }
+]);
